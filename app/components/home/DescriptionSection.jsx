f81f7a1fbc29bfc2a8ea7d30f75e169cad828bbc
Simona Zozuľaková <szozulakova@gmail.com>
2023-05-03 21:36:36 +0200

    about me

---
+++ app/components/home/DescriptionSection.jsx
@@ -0,0 +1,104 @@
+"use client";
+import Container from "../Container";
+import styled from "styled-components";
+import ButtonExtra from "../ButtonExtra";
+
+const DescriptionWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+
+  .contentWrapper {
+    display: flex;
+    padding: 50px 0;
+    position: relative;
+    z-index: 999;
+    max-width: 640px;
+
+    .description {
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+      gap: 1rem;
+    }
+    .calculations {
+      display: flex;
+      justify-content: space-evenly;
+      .calculation {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+    }
+    .imageWrapper {
+      display: flex;
+      justify-content: center;
+      img {
+        position: relative;
+        object-fit: cover;
+        height: 30rem;
+        width: 25rem;
+        z-index: 9999;
+        border-radius: 4rem;
+      }
+    }
+  }
+  @media (max-width: 1000px) {
+    .contentWrapper {
+      gap: 3rem;
+      .introtext {
+        align-items: center;
+        text-align: center;
+      }
+    }
+  }
+  @media (max-width: 500px) {
+    .contentWrapper {
+      .imageWrapper {
+        img {
+          height: 20rem;
+          width: 15rem;
+        }
+      }
+    }
+  }
+`;
+
+const DescriptionSection = () => {
+  return (
+    <DescriptionWrapper>
+      <Container>
+        <div className="contentWrapper">
+          <div className="description">
+            <h2>I am a FullStack developer with about 1 year of experience.</h2>
+            <p>
+              I'm a 20-year-old FullStack programmer. I started programming more
+              than a year ago. Since then, I have been expanding my knowledge
+              and continue to do so. I currently live in Trnava, but I come from
+              Svidník, a city in eastern Slovakia.
+            </p>
+            <div className="calculations">
+              <div className="calculation">
+                <h3>10</h3>
+                <p>Projects completed</p>
+                <ButtonExtra>
+                  <h3>Contact</h3>
+                </ButtonExtra>
+              </div>
+              <div className="calculation">
+                <h3>1000+</h3>
+                <p>lines of code writte</p>
+                <ButtonExtra>
+                  <h3>Portfolio</h3>
+                </ButtonExtra>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </DescriptionWrapper>
+  );
+};
+
+export default DescriptionSection;
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import IntroSection from "./components/home/IntroSection";
+import DescriptionSection from "./components/home/DescriptionSection";
 
 const Home = () => {
   return (
     <div>
       <IntroSection />
+      <DescriptionSection />
     </div>
   );
 };
